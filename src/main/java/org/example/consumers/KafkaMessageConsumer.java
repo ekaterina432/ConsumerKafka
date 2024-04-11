be873b95e3ca83a1bedb9aa145d89613b78a461b
KatePrish <prishchepa@sfedu.ru>
2024-04-11 11:45:30 +0300

    logger

--- src/main/java/org/example/consumers/KafkaMessageConsumer.java
+++ src/main/java/org/example/consumers/KafkaMessageConsumer.java
@@ -1,13 +1,15 @@
 package org.example.consumers;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaMessageConsumer {
-
+private static final Logger log = LoggerFactory.getLogger(KafkaMessageConsumer.class);
     @KafkaListener(topics = "devglan-partitions-topic", groupId = "test-group")
     public void listen(ConsumerRecord<String, String> record) {
-        System.out.println(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
+        log.info(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
     }
 }
